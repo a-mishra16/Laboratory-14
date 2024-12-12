7eb54c7b0de256420d7e132fabcd2abd0332bf3a
Aanika Mishra <a-mishra@onu.edu>
2024-12-12 08:28:06 -0500

    Created prototypes and skeletons of functions

--- Laboratory 14.cpp
+++ Laboratory 14.cpp
@@ -4,3 +4,31 @@
 * Date: 12-12-2024
 * Description: This program reads in a user specified number of floating-point values
 */
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double average(double* a, int size);
+double* maximum(double* a, int size);
+double* minimum(double* a, int size);
+
+int main()
+{
+	int arraySize = 0;
+
+}
+
+double average(double* a, int size)
+{
+	
+}
+double* maximum(double* a, int size)
+{
+
+}
+double* minimum(double* a, int size)
+{
+
+}
